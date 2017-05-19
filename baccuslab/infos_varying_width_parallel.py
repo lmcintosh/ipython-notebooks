e2df8cdd4c17da300d6b5b0dab2ddf6459ede786
lmcintosh <lmcintosh@stanford.edu>
2017-05-19 10:43:32 -0700

    adding script to parallelize infos_varying_width

---
+++ baccuslab/infos_varying_width_parallel.py
@@ -0,0 +1,233 @@
+import matplotlib
+matplotlib.use('Agg')
+
+import numpy as np
+import collections
+from functools import partial
+from os.path import expanduser
+from tqdm import tqdm
+import h5py
+
+from surround.image_processing import *
+from surround.data_handling import *
+from surround.efficient_coding import *
+from aesthetics.plotting import *
+from surround.modeling import gaussian, difference_of_gaussians
+
+import pyret.filtertools as ft
+from sklearn.decomposition import PCA
+from scipy.stats import sem
+from scipy.optimize import curve_fit, minimize
+from scipy.interpolate import interp1d
+from scipy.stats import pearsonr
+
+from scipy.signal import convolve2d
+import datetime
+
+from joblib import Parallel, delayed
+import multiprocessing
+
+todays_date = str(datetime.date.today())
+print(todays_date)
+
+# parameters
+input_noise = 0.0469253741641
+output_noise = 0.35
+target_power = 48.053365503112332
+widths = np.linspace(0.01, 17, 100)
+init_filt_const = 10.9
+resolution = 100
+horz_weights = np.linspace(0,1,resolution)
+center_weights = np.linspace(0,1,resolution)
+
+# some necessary pre-processing
+
+# load data
+# These are loaded as (space, receptive) field tuples
+cells = {}
+cells['ganglion'] = load_ganglion_cells(space_mode='peak')
+cells['bipolar'] = load_bipolar_cells(space_mode='peak')
+cells['amacrine'] = load_amacrine_cells(space_mode='peak')
+cells['horizontal'] = load_horizontal_cells(space_mode='peak')
+receptive_fields = collections.defaultdict(list)
+for c in cells.keys():
+    for s, rf in cells[c]:
+        this_cell_interp = interp1d(s, rf, kind='slinear', bounds_error=False, fill_value=[0])
+        receptive_fields[c].append(this_cell_interp(space))
+        
+average_ganglion_rf = np.mean(receptive_fields['ganglion'], axis=0)
+
+data_path = expanduser('~/data/kastner/receptive_fields/')
+spatialDelta = 0.05 # mm
+micronsPerDeg = 50.
+ganglion_rfs = {
+    'fast_on': np.loadtxt(data_path + 'linesRF1.txt').reshape((126, 101, -1)),
+    'fast_off_adapting': np.loadtxt(data_path + 'linesRF19.txt').reshape((-1, 101, 96)),
+    'fast_off_sensitizing': np.loadtxt(data_path + 'linesRF8.txt').reshape((-1, 101, 96)),
+    'medium_off_adapting': np.loadtxt(data_path + 'linesRF29.txt').reshape((-1, 101, 96)),
+    'slow_off': np.loadtxt(data_path + 'linesRF17.txt').reshape((-1, 101, 96)),
+}
+
+for celltype in ganglion_rfs.keys():
+    for rf in ganglion_rfs[celltype]:
+        # since receptive fields are noisy, use PCA
+        pca = PCA(n_components=2)
+        pca.fit(rf)
+        g_pca = pca.components_[0]
+
+        sign_of_pc = -1 * np.sign(g_pca[abs(g_pca) == np.max(abs(g_pca))])
+        this_space = get_space(g_pca, spatialDelta, micronsPerDeg, kind='peak')
+        
+        rf_interp = interp1d(this_space, sign_of_pc*g_pca, kind='slinear', bounds_error=False, fill_value=[0])
+
+        receptive_fields[celltype].append(rf_interp(space))
+
+corr_map = {}
+for celltype in receptive_fields.keys():
+    corr_map[celltype] = np.zeros((len(receptive_fields[celltype]), len(receptive_fields[celltype])))
+    for idr1, rf1 in tqdm(enumerate(receptive_fields[celltype])):
+        for idr2 in range(idr1+1, len(receptive_fields[celltype])):
+            rf2 = receptive_fields[celltype][idr2]
+            corr_map[celltype][idr1, idr2] = pearsonr(rf1, rf2)[0]
+
+unduplicated_receptive_fields = collections.defaultdict(list)
+duplicates = collections.defaultdict(list)
+
+for celltype in corr_map.keys():
+    these_dups = []
+    for row in corr_map[celltype]:
+        these_dups.append([i for i in range(len(row)) if row[i] == 1.0])
+    duplicates[celltype] = np.unique(np.hstack(these_dups))
+    
+unduplicated_receptive_fields = collections.defaultdict(list)
+for celltype in receptive_fields.keys():
+    for idr, rf in enumerate(receptive_fields[celltype]):
+        if idr not in duplicates[celltype]:
+            unduplicated_receptive_fields[celltype].append(rf)
+            
+receptive_fields = unduplicated_receptive_fields.copy()
+del unduplicated_receptive_fields
+print('Done with loading data')
+
+# Compute median center width
+diff_of_gauss_mu0 = partial(difference_of_gaussians, mu=0)
+def center_and_surround(space, center_width, surround_width, center_strength, surround_strength):
+    return diff_of_gauss_mu0(space, abs(center_width), abs(surround_width),
+                            -abs(center_strength), abs(surround_strength))
+
+center_widths = collections.defaultdict(list)
+surround_widths = collections.defaultdict(list)
+center_strengths = collections.defaultdict(list)
+surround_strengths = collections.defaultdict(list)
+fit_failures = collections.defaultdict(list)
+
+for celltype in receptive_fields.keys():
+    if celltype in ['ganglion', 'fast_on', 'fast_off_adapting', 'fast_off_sensitizing', 
+                    'medium_off_adapting', 'slow_off']:
+        
+        for idg, g in tqdm(enumerate(receptive_fields[celltype])):
+            try:
+                popt_this, pcov = curve_fit(center_and_surround, space, g, p0=[1.5, 3.5, -10, 30])
+                center_id = np.argmin(abs(popt_this[:2]))
+                surround_id = 1 ^ center_id
+                center_widths[celltype].append(abs(popt_this[center_id]))
+                surround_widths[celltype].append(abs(popt_this[surround_id]))
+                center_strengths[celltype].append(popt_this[center_id+2])
+                surround_strengths[celltype].append(popt_this[surround_id+2])
+#                 plt.plot(space, diff_of_gauss_mu0(space, *popt_this), color=np.random.rand(3), alpha=0.6)
+            except:
+                fit_failures[celltype].append(idg)
+            
+                # just set params to median across cells
+                # this way the params are still aligned with the receptive field id
+                center_widths[celltype].append(np.median(center_widths[celltype]))
+                surround_widths[celltype].append(np.median(surround_widths[celltype]))
+                center_strengths[celltype].append(np.median(center_strengths[celltype]))
+                surround_strengths[celltype].append(np.median(surround_strengths[celltype]))
+                
+#                 print("Couldn't fit cell %d" %idg)
+
+median_center_widths = {}
+for celltype in center_widths.keys():
+    cws = np.array(center_widths[celltype])
+    sws = np.array(surround_widths[celltype])
+    css = np.array(center_strengths[celltype])
+    sss = np.array(surround_strengths[celltype])
+#     mean_center_widths[celltype] = np.mean(cws[cws < 500])
+#     mean_surround_width = np.mean(sws[sws < 500])
+#     mean_center_strength = np.mean(css[abs(css) < 500])
+#     mean_surround_strength = np.mean(sss[abs(sss) < 500])
+    
+    median_center_widths[celltype] = np.median(abs(cws))
+    median_surround_width = np.median(abs(sws))
+    median_center_strength = np.median(-abs(css))
+    median_surround_strength = np.median(abs(sss))
+#     plt.plot(space, diff_of_gauss_mu0(space, median_center_widths[celltype], median_surround_width,
+#                                      median_center_strength, median_surround_strength), label=celltype)
+# plt.legend(frameon=False, fontsize=9, loc='lower left')
+# adjust_spines(plt.gca())
+print('Done with computing median center widths')
+
+
+
+celltype = 'fast_off_adapting'
+#info_maps_by_widths = collections.defaultdict(list)
+center = gaussian(x=space, sigma=median_center_widths[celltype], mu=0.)
+center /= -np.sum(center)
+
+
+#for idw1, width1 in enumerate(widths):
+def processWidth(idw1, width1):
+    these_infomaps = collections.defaultdict(list)
+    surround1 = gaussian(x=space, sigma=width1, mu=0.)
+    surround1 /= np.sum(surround1)
+    for idw2, width2 in tqdm(enumerate(widths[idw1:])):
+        surround2 = gaussian(x=space, sigma=width2, mu=0.)
+        surround2 /= np.sum(surround2)
+
+        def rf_model(horz_weight, center_weight):
+            return center_weight*center + (1-center_weight)*(horz_weight*surround2 + (1-horz_weight)*surround1)
+
+        infomap = np.zeros((resolution, resolution))
+        for idh,hw in enumerate(horz_weights):
+            for idc,cw in enumerate(center_weights):
+                rf = rf_model(hw, cw)
+                rf_filt = abs(np.fft.rfft(rf))
+
+                # constrain model
+                def constrain_filt_power(filt_const):
+                    size = len(rf_filt)
+                    output_power = np.sum((signal*filt_const*rf_filt)**2 
+                                          + (input_noise*filt_const*rf_filt)**2
+                                          + output_noise**2)
+
+                    return (target_power - output_power)**2
+
+                filt_const_opt = scipy.optimize.minimize(constrain_filt_power, init_filt_const)
+                iterations = 0
+                new_init_filt_const = init_filt_const
+                while not filt_const_opt.success:
+                    iterations += 1
+                    new_init_filt_const *= 10
+                    filt_const_opt = scipy.optimize.minimize(constrain_filt_power, new_init_filt_const)
+                    if iterations > 10:
+                        detailed_failures[celltype] = filt_const_opt
+                        break
+
+                filt_const = abs(filt_const_opt['x'])
+
+                if not filt_const_opt.success:
+                    failures[celltype].append([hw, cw])
+
+                signal_power = (filt_const * rf_filt * signal)**2
+                noise_power = (filt_const * rf_filt * input_noise)**2 + output_noise**2
+                infomap[idh,idc] = 0.5*np.sum(np.log2(1 + signal_power/noise_power))
+        these_infomaps[(width1, width2)] = infomap
+    with h5py.File('%s_%d_%f_these_infomaps.h5' %(todays_date, idw1, width1), 'w') as f:
+        for key in these_infomaps.keys():
+            f.create_dataset('%s' %str(key), data=these_infomaps[key])
+
+    print('Done with width number %d' %idw1)
+
+num_cores = multiprocessing.cpu_count()
+Parallel(n_jobs=num_cores)(delayed(processWidth)(idw1, width1) for idw1, width1 in enumerate(widths))
